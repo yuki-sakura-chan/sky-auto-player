ecd5438986ed593ced706182cd4a6cca485b7b1f
Yuki_Sakura <YukiSakura2466@gmail.com>
2024-07-18 11:54:41 +0800

    :wrench: 添加配置管理包

---
+++ sakura/config/Config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import os
+
+import yaml
+
+
+class Config:
+    file_path = ''
+    mapping = {}
+    player = {}
+
+    @staticmethod
+    def load_yaml_config():
+        with open(os.path.join(os.getcwd(), 'config.yaml'), 'r') as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+            conf.file_path = config['file_path']
+            conf.mapping = config['mapping']
+            conf.player = config['player']
+
+
+conf = Config()
