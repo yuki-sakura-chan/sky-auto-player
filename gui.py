f5835729a1a44c131ffe8e00c37b4bcb89580903
Yuki_Sakura <YukiSakura2466@gmail.com>
2024-09-17 22:36:21 +0800

    :sparkles: 添加gui

---
+++ gui.py
@@ -0,0 +1,52 @@
+import sys
+
+from PySide6.QtGui import QIcon, Qt
+from PySide6.QtWidgets import QApplication, QHBoxLayout, QFrame
+from qfluentwidgets import NavigationItemPosition, FluentWindow, SubtitleLabel, setFont
+from qfluentwidgets import FluentIcon as FIF
+
+from sakura.components.ui.Home import Home
+
+
+class Widget(QFrame):
+
+    def __init__(self, text: str, parent=None):
+        super().__init__(parent=parent)
+        self.label = SubtitleLabel(text, self)
+        self.hBoxLayout = QHBoxLayout(self)
+
+        setFont(self.label, 24)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.hBoxLayout.addWidget(self.label, 1, Qt.AlignCenter)
+
+        # 必须给子界面设置全局唯一的对象名
+        self.setObjectName(text.replace(' ', '-'))
+
+
+class Window(FluentWindow):
+    """ 主界面 """
+
+    def __init__(self):
+        super().__init__()
+
+        # 创建子界面，实际使用时将 Widget 换成自己的子界面
+        self.homeInterface = Home(self)
+        self.settingInterface = Widget('Setting Interface', self)
+        self.init_navigation()
+        self.init_window()
+
+    def init_navigation(self):
+        self.addSubInterface(self.homeInterface, FIF.HOME, 'Home')
+        self.addSubInterface(self.settingInterface, FIF.SETTING, 'Settings', NavigationItemPosition.BOTTOM)
+
+    def init_window(self):
+        self.resize(1024, 768)
+        self.setWindowIcon(QIcon(':/qfluentwidgets/images/logo.png'))
+        self.setWindowTitle('Sky Auto Player')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = Window()
+    w.show()
+    app.exec()
---
+++ sakura/components/ui/Home.py
@@ -0,0 +1,60 @@
+import webbrowser
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QFrame, QVBoxLayout, QSpacerItem, QHBoxLayout, QSizePolicy
+from qfluentwidgets import FlowLayout, LargeTitleLabel, ElevatedCardWidget, SubtitleLabel, CaptionLabel, IconWidget, \
+    FluentIcon, ToolButton
+
+
+class Home(QFrame):
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.setObjectName("Home")
+        layout = QVBoxLayout(self)
+        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        layout.setContentsMargins(30, 20, 30, 20)
+        layout.addWidget(LargeTitleLabel('Welcome to Sky Auto Player!', None))
+        home_card = HomeCard('GitHub repo', '本程序是免费开源的，点击可以跳转到GitHub仓库', FluentIcon.GITHUB,
+                             'https;//github.com/yuki-sakura-chan/sky-auto-player', self)
+        body_layout = FlowLayout()
+        body_layout.addWidget(home_card)
+        layout.addLayout(body_layout)
+
+
+class HomeCard(ElevatedCardWidget):
+
+    def __init__(self, title: str, text: str, icon: FluentIcon, url: str, parent=None):
+        super().__init__(parent=parent)
+        self.adjustSize()
+        self.setFixedWidth(200)
+        icon = IconWidget(icon, self)
+        icon.setFixedSize(48, 48)
+        title_label = SubtitleLabel(title, self)
+        text_label = CaptionLabel(text, self)
+        text_label.setWordWrap(True)
+
+        link = ToolButton(FluentIcon.LINK, self)
+        link.setFixedSize(16, 16)
+        link.setCursor(Qt.CursorShape.PointingHandCursor)
+        link.clicked.connect(lambda: webbrowser.open(url))
+
+        # 创建主布局
+        layout = QVBoxLayout(self)
+        layout.setContentsMargins(20, 20, 20, 20)
+
+        # 添加控件到布局
+        layout.addWidget(icon)
+        layout.addWidget(title_label)
+        layout.addWidget(text_label)
+
+        # 创建一个较小的占位的 QSpacerItem 用于占用空间，使 link 靠底部对齐
+        spacer = QSpacerItem(20, 10, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        layout.addItem(spacer)
+
+        # 添加 link 按钮并设置其靠右对齐
+        link_layout = QHBoxLayout()
+        link_layout.addStretch()
+        link_layout.addWidget(link)
+
+        layout.addLayout(link_layout)
