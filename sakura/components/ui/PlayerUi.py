920c79e2e4bfa6c864ed09a69020dddd951ab395
Yuki_Sakura <YukiSakura2466@gmail.com>
2024-09-19 22:30:42 +0800

    :sparkles: 加载文件列表

--- sakura/components/ui/PlayerUi.py
+++ sakura/components/ui/PlayerUi.py
@@ -1,6 +1,9 @@
 from PySide6.QtWidgets import QFrame
 from qfluentwidgets import FlowLayout, ListWidget
 
+from main import get_file_list
+from sakura.config.Config import conf
+
 
 class PlayerUi(QFrame):
     def __init__(self, parent=None):
@@ -8,5 +11,7 @@ class PlayerUi(QFrame):
         self.setObjectName("Player")
         main_layout = FlowLayout(self)
         file_list_box = ListWidget()
-        file_list_box.addItem("File 1")
+        file_list = get_file_list(conf.get('file_path'))
+        for index, file in enumerate(file_list):
+            file_list_box.addItem(file)
         main_layout.addWidget(file_list_box)
