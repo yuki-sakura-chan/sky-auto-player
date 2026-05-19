aec4898c42968e3ac1832ba70bce46dab1ed8d47
Yuki_Sakura <caiyucong12@gmail.com>
2026-05-20 01:34:23 +0800

    :hammer: 更新build文件

--- build-gui.bat
+++ build-gui.bat
@@ -4,4 +4,20 @@ set COMPANY_NAME=Yuki_Sakura
 set PRODUCT_NAME=Sky-auto-player
 set FILE_VERSION=1.0.0
 
-nuitka --standalone --output-filename=%FILENAME% --output-dir=dist --company-name=%COMPANY_NAME% --product-name=%PRODUCT_NAME% --file-version=%FILE_VERSION% --assume-yes-for-downloads --follow-imports --include-package=sakura --include-data-dir=resources=resources --include-data-file=config.yaml=config.yaml --windows-icon-from-ico=resources/static/icon/logo-64x64.ico --windows-console-mode=disable --enable-plugin=pyside6 --mingw64 .\gui.py
+nuitka ^
+--standalone ^
+--output-dir=dist ^
+--output-filename=%FILENAME% ^
+--company-name=%COMPANY_NAME% ^
+--product-name=%PRODUCT_NAME% ^
+--file-version=%FILE_VERSION% ^
+--include-package=sakura ^
+--assume-yes-for-downloads ^
+--include-data-dir=resources=resources ^
+--include-data-file=config.yaml=config.yaml ^
+--windows-icon-from-ico=resources/static/icon/logo-64x64.ico ^
+--windows-console-mode=disable ^
+--enable-plugin=pyside6 ^
+--include-qt-plugins=sensible ^
+--msvc=latest ^
+.\gui.py
